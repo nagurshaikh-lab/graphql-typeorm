54295417742b40a919466626955ae8a15bd41a9c
NAGUR <nagur.shaikh@hotmail.com>
2021-02-11 16:37:43 +0530

    removed unwanted code

--- src/repositories/client-repository.ts
+++ src/repositories/client-repository.ts
@@ -1,14 +1,9 @@
 import { Resolver, Query, Arg } from "type-graphql";
-import { Connection, createConnection, getConnection } from "typeorm";
+import { Connection, getConnection } from "typeorm";
 
-import { CaddyClient } from "../entities/client-entities/caddy-client";
+import { Client } from "../entities/client-entities/client";
 import { Building } from "../entities/facility-info/building";
 import { Location } from '../entities/facility-info/location';
-import { StorageType } from "../entities/facility-info/storage-type";
-import { UnitInfo } from "../entities/facility-info/unit-info";
-import { UnitType } from "../entities/facility-info/unit-type";
-import { UnitsRequest } from "../request-models/units-request";
-import { SignUpInsights } from "../response-models/signup-upgrade-resposne";
 
 @Resolver()
 export class CaddyClientRepository {
@@ -21,20 +16,16 @@ export class CaddyClientRepository {
     this._connection = getConnection("default");
   }
 
-  @Query(() => [CaddyClient])
+  @Query(() => [Client])
   clients() {
-    return CaddyClient.find({order: { accountId:"DESC" }});
+    return Client.find({order: { accountId:"DESC" }});
   }
 
-  @Query(() => CaddyClient, { name: 'account' })
+  @Query(() => Client, { name: 'account' })
   client(@Arg("accountId", { nullable: true }) accountId?: Number, @Arg("clientId", { nullable: true }) clientId?: String) {
-    return CaddyClient.findOne({ where: [{ accountId, deleted: false }, { clientId, deleted: false }], cache: true, relations: ['locations'] });
+    return Client.findOne({ where: [{ accountId, deleted: false }, { clientId, deleted: false }], cache: true, relations: ['locations'] });
   }
 
-  @Query(() => [StorageType], { name: 'storageTypes' })
-  storageTypes(@Arg('clientId') clientId: String) {
-    return StorageType.find({ where: { clientId: clientId, deleted: false }, cache: true, relations: ['units'] });
-  }
 
   @Query(() => [Location], { name: 'location' })
   locations(@Arg('clientId') clientId: String) {
@@ -44,33 +35,5 @@ export class CaddyClientRepository {
   @Query(() => [Building], { name: 'building' })
   buildings(@Arg('locationId') locationId: String) {
     return Building.find({ where: { locationId: locationId, deleted: false }, cache: true, relations: ['location', 'units'] });
-  }
-
-  @Query(() => [UnitType], { name: 'unitTypes' })
-  unitTypes(@Arg('clientId') clientId: String) {
-    return UnitType.find({ where: { clientId: clientId, deleted: false }, cache: true, relations: ['units'] });
-  }
-
-  @Query(() => [UnitInfo], { name: 'units' })
-  units(@Arg("request", { nullable: true, validate: true }) request: UnitsRequest) {
-    return UnitInfo.find({
-      where: [
-        { storageTypeID: request.storageTypeId, deleted: false, clientId: request.clientId },
-        { unitTypeID: request.unitTypeId, deleted: false },
-        { buildingId: request.buildingId, deleted: false },
-      ],
-      cache: true, relations: ['building', 'storageType', 'unitType']
-    });
-  }
-
-  @Query(() => [SignUpInsights], { name: 'signupInsights' })
-  async getSignUpDetails(@Arg('year') year: Number): Promise<SignUpInsights[]> {
-    debugger;
-    let data = await this._connection.manager.query('EXECUTE usp_gp_v1_SignUpsInfo_get @0', [year]) as Promise<SignUpInsights[]>;
-    // .then((dataset:any):Promise<SignUpInsights[]> => {
-    //   return dataset as Promise<SignUpInsights[]>;
-    // });
-    console.log(data);
-    return data;
   }
 }
